0cc12c1352e74a6eef61d4daf74a69e3a807d914
rifqimaulana05 <rifkybolang50@gmail.com>
2025-11-26 17:30:10 +0800

    update MVC

--- model.py
+++ model.py
@@ -19,7 +19,7 @@ class Database:
         self.cursor = self.conn.cursor()
         self.buat_tabel()
 
-    def buat_tabel():
+    def buat_tabel(self):
         self.cursor.execute("""
             CREATE TABLE IF NOT EXISTS mahasiswa (
                 id TEXT PRIMARY KEY,
@@ -48,38 +48,38 @@ class Database:
 
         self.conn.commit()
 
-def tambah_mahasiswa(self, id, nama, jurusan):
-    self.cursor.execute("INSERT INTO mahasiswa VALUES (?, ?, ?)", (id, nama, jurusan))
-    self.conn.commit()
+    def tambah_mahasiswa(self, id, nama, jurusan):
+        self.cursor.execute("INSERT INTO mahasiswa VALUES (?, ?, ?)", (id, nama, jurusan))
+        self.conn.commit()
 
-def get_mahasiswa(self):
-    self.cursor.execute("SELECT * FROM mahasiswa")
-    return self.cursor.fetchall()
+    def get_mahasiswa(self):
+        self.cursor.execute("SELECT * FROM mahasiswa")
+        return self.cursor.fetchall()
 
-def update_mahasiswa(self, id, nama, jurusan):
-    self.cursor.execute("UPDATE mahasiswa SET nama=?, jurusan=? WHERE id=?", (nama, jurusan, id))
-    self.conn.commit()
+    def update_mahasiswa(self, id, nama, jurusan):
+        self.cursor.execute("UPDATE mahasiswa SET nama=?, jurusan=? WHERE id=?", (nama, jurusan, id))
+        self.conn.commit()
 
-def delete_mahasiswa(self, id):
-    self.cursor.execute("DELETE FROM mahasiswa WHERE id=?", (id,))
-    self.conn.commit()
-
-
-
-def tambah_dosen(self, id, nama, matkul):
-    self.cursor.execute("INSERT INTO dosen VALUES (?, ?, ?)", (id, nama, matkul))
-    self.conn.commit()
-
-def get_dosen(self):
-    self.cursor.execute("SELECT * FROM dosen")
-    return self.cursor.fetchall()
+    def delete_mahasiswa(self, id):
+        self.cursor.execute("DELETE FROM mahasiswa WHERE id=?", (id,))
+        self.conn.commit()
 
 
-def beri_nilai(self, mahasiswa_id, matkul, nilai):
-    self.cursor.execute("INSERT INTO nilai(mahasiswa_id, mata_kuliah, nilai) VALUES (?, ?, ?)",
+
+    def tambah_dosen(self, id, nama, matkul):
+        self.cursor.execute("INSERT INTO dosen VALUES (?, ?, ?)", (id, nama, matkul))
+        self.conn.commit()
+
+    def get_dosen(self):
+        self.cursor.execute("SELECT * FROM dosen")
+        return self.cursor.fetchall()
+
+
+    def beri_nilai(self, mahasiswa_id, matkul, nilai):
+        self.cursor.execute("INSERT INTO nilai(mahasiswa_id, mata_kuliah, nilai) VALUES (?, ?, ?)",
                 (mahasiswa_id, matkul, nilai))
-    self.conn.commit()
+        self.conn.commit()
 
-def get_nilai(self, mahasiswa_id):
-    self.cursor.execute("SELECT mata_kuliah, nilai FROM nilai WHERE mahasiswa_id=?", (mahasiswa_id,))
-    return self.cursor.fetchall()
+    def get_nilai(self, mahasiswa_id):
+        self.cursor.execute("SELECT mata_kuliah, nilai FROM nilai WHERE mahasiswa_id=?", (mahasiswa_id,))
+        return self.cursor.fetchall()
